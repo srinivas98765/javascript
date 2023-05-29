1ea57c14378f20ce7bfdba700ead3904f4cc06d9
Srinivase1204@ <55191480+srinivas98765@users.noreply.github.com>
2023-05-29 12:18:07 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,8 +1,18 @@
 core modules in 
   Fs,Buffer,Http
 *************************************
+
   const http =require('http');
 http.createServer((req,res)=>{
     res.write("<h1>Hello this is this</h1>");
     res.end();
 }).listen(4500);
+
+
+const http =require('http');
+function dataControl(req,res)
+{
+    res.write("hello this is this");
+    res.end();
+}
+http.createServer(dataControl).listen(4500);
