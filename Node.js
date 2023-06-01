35b07eda4edcd4bc4d52f47aba83e36b60ce8cca
Srinivase1204@ <55191480+srinivas98765@users.noreply.github.com>
2023-06-01 21:50:35 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -16,3 +16,17 @@ function dataControl(req,res)
     res.end();
 }
 http.createServer(dataControl).listen(4500);
+*******************************************
+  data.js
+const data={name:'Anil sidhu',email:'qwert@gmail.com'}
+module.exports=data;
+index.js
+const http=require('http');
+const data=require('./data');
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'content-type':'application\json'});
+    resp.write(JSON.stringify(data));
+    resp.end();
+}).listen(5000);
+********************************************
+  
