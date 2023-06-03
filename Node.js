7914137b4ae2145f94298975572d7300515b09fd
Srinivase1204@ <55191480+srinivas98765@users.noreply.github.com>
2023-06-03 11:21:10 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -29,4 +29,19 @@ http.createServer((req,resp)=>{
     resp.end();
 }).listen(5000);
 ********************************************
+  const fs=require('fs');
+const input=process.argv;
+if(input[2]=='add')
+{
+fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]=='remove')
+{
+fs.unlinkSync(input[3]);
+}
+else{
+    console.log("Invalid input");
+}
+input:node index.js add orange.txt 'this is is the file'
+**********************************************
   
